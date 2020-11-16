7b489d406f2783759dae3d6650b6eb468f377b60
DeekshaReddyM <69183696+DeekshaReddyM@users.noreply.github.com>
2020-11-16 13:56:14 +0530

    Add files via upload

---
+++ ClinicBill.java
@@ -0,0 +1,45 @@
+package com.clinicalmgmt.bill;
+/**
+ * Example for Implementing Interface
+ * @author ADMIN
+ *
+ */
+public class ClinicBill implements Bill {
+
+	/* (non-Javadoc)
+	 * @see Bill#createBill()
+	 */
+	@Override
+	public void createBill() {
+		System.out.println("Create a Bill Ob");
+
+	}
+
+	/* (non-Javadoc)
+	 * @see Bill#modifyBill()
+	 */
+	@Override
+	public void modifyBill() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see Bill#printBill()
+	 */
+	@Override
+	public void printBill() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see Bill#sendBill()
+	 */
+	@Override
+	public void sendBill() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
